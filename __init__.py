2755198c739f9733973a06e72d3419b1097ae453
puckipsi <angelskijm@gmail.com>
2023-03-31 00:56:52 +0300

    add page for viewing uploads

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-import os
+import os, os.path, time
 from flask import Flask, render_template, request, send_file
 from config import get_current_datetime
 
@@ -23,7 +23,24 @@ def upload():
 	return render_template('download.html', file=full_path, filename=filename, datetime=datetime)
 
 
+@app.route('/view-uploads',methods=['GET'])
+def uploaded_list():
+	files = get_files()
+	return render_template('display.html', files=files)
+
+
 @app.route('/download/<file>', methods=["GET"])
 def download(file):
     file_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'],file)
     return send_file(file_path, as_attachment=True)
+
+
+
+def get_files():
+	dir_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+	dir_files = os.listdir(dir_path)
+	files = [{
+		'file': file,
+	    'upload_date':time.ctime(os.path.getctime(f"{dir_path}/{file}"))} for file in dir_files]  
+	
+	return files
